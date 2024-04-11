fb29dbe4ce2e0017f6c2629384260d4cdeb78045
teppei <tetukawano1126@gmail.com>
2024-04-11 22:11:40 +0900

    update config

--- config/config.py
+++ config/config.py
@@ -9,18 +9,7 @@ class SystemConfig:
     DEBUG = True
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI_LOCAL")
-    #     (
-    #         "mysql+pymysql://{user}:{password}@{host}/{db-name}?charset=utf8".format(
-    #             **{
-    #                 "user": "root",
-    #                 "password": "Password_123",
-    #                 "host": "localhost",
-    #                 "db-name": "py_portfolio",
-    #             }
-    #         )
-    #     )
-    # "mysql+pymysql://root:Password_123@localhost/py_portfolio?charset=utf8"
-    SECRET_KEY = "super-secret"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
     REMEMBER_COOKIE_SAMESITE = "strict"
     SESSION_COOKIE_SAMESITE = "strict"
